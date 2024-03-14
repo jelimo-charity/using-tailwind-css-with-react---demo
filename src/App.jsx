1655bc14d5b205b6e81b5351e0c90b861f46b2b3
jelimo-charity <charityjelimo893@gmail.com>
2024-03-14 21:50:30 +0300

    tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world charity!</h1> 
-    </>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
